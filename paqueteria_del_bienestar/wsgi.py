b325ee55845310e7a258b1aaad7d6268beb77a89
root <root@vmi791577.contaboserver.net>
2024-11-23 11:25:43 -0800

    configramos todo para la vps únicamente

--- paqueteria_del_bienestar/wsgi.py
+++ paqueteria_del_bienestar/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for paquetería_del_bienestar project.
+WSGI config for paqueteria_del_bienestar project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paquetería_del_bienestar.settings')
+# Ajuste del nombre del módulo para evitar problemas con codificación o caracteres especiales
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paqueteria_del_bienestar.settings')
 
 application = get_wsgi_application()
