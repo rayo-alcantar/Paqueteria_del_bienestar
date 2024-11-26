4d96cfe33526ec4889b04691388a5fa20ff4aa2f
Angel Alcantar <rayoalcantar@gmail.com>
2024-11-26 07:12:55 -0600

    se pone el url faq en ulr

--- paquetes/urls.py
+++ paquetes/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
 	path('rastreo-paquete/', views.rastrear_paquete, name='rastreo_paquete'),  # Página de rastreo de paquete
 	path('descargar-pdf/<str:codigo>/', views.descargar_pdf, name='descargar_pdf'),
 	path('solicitar-envio/', views.solicitar_envio, name='solicitar_envio'),  # Página para solicitar envío
-	path('acerca-de/', views.acerca_de, name='acerca_de'),  # Página "Acerca de"
+	path('acerca-de/', views.acerca_de, name='acerca_de'),  # Página "Acerca de",
+	path('FAQ/', views.FAQ, name='FAQ'),  # Página "Acerca de",
 ]
